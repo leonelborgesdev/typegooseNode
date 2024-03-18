c1096cfb0c232a193203510c8ee11b631b5e991e
leonelborgesdev <leonel03t@gmail.com>
2024-03-18 14:00:19 -0400

    src/index mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello world2")
+import mongoose from "mongoose";
+
+async function connectDB() {
+    const db = await mongoose.connect('mongodb://localhost/typegoosedb');
+    console.log('database is connected to', db.connection.db.databaseName);
+}
+
+connectDB();
